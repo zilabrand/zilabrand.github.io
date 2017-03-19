a8374a97bd914d6f2a956101236676acfecfc99d
Giaco Corsiglia <GiacoCorsiglia@gmail.com>
2017-03-19 16:02:28 -0700

    Revert to compiling assets into root
    GitHub pages doesn’t seem to like keep_files

--- _assets/webpack.config.js
+++ _assets/webpack.config.js
@@ -9,8 +9,8 @@ module.exports = {
 		app: './index.js',
 	},
 	output: {
-		path: path.resolve(__dirname, '../_site/assets/'),
-		filename: 'script.js',
+		path: path.resolve(__dirname, '../'),
+		filename: '_script.js',
 	},
 	module: {
 		rules: [
@@ -35,7 +35,7 @@ module.exports = {
 		],
 	},
 	plugins: [
-		new ExtractTextPlugin('style.css')
+		new ExtractTextPlugin('_style.css')
 	],
 	// Need this for some bug nonsense
 	node: {
