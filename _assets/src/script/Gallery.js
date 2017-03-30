cd45bac80bdc86d6d0f702a3038ec7d7474b7858
Giaco Corsiglia <GiacoCorsiglia@gmail.com>
2017-03-30 13:56:25 -0700

    Gallery improvements

--- _assets/src/script/Gallery.js
+++ _assets/src/script/Gallery.js
@@ -7,8 +7,27 @@ galContainer.className = 'gal-container'
 const galContent = document.createElement('div')
 galContent.className = 'gal-content'
 
+galContainer.appendChild(galContent)
+
+const closeButton = document.createElement('div')
+closeButton.className = 'gal-close'
+
+galContainer.appendChild(closeButton)
+
+let close = function(){
+  galContainer.remove()
+  galContent.innerHTML = ''
+}
+
+closeButton.addEventListener('click', close)
+galContainer.addEventListener('click', (event) => {
+  if(event.target === galContainer){
+    close()
+  }
+})
+
 export function createGallery(el) {
-  let show = function(index) {
+  let show = function(index){
     let slides = map(galItems, () => {
       let slide = document.createElement('div')
       slide.className = 'slide'
@@ -16,21 +35,21 @@ export function createGallery(el) {
       return slide
     })
 
-    initSlideshow(galContent, index, 500, (to) => {
-      let slide = slides[to]
-      if(!slide.getElementsByTagName('img')){
-        let img = new Image()
-        img.src = galItems[to].src
-        slide.appendChild(img)
+    initSlideshow({
+      el: galContent,
+      start: index,
+      duration: 500,
+      transitionCallback(to) {
+        let slide = slides[to]
+        if(!slide.getElementsByTagName('img').length){
+          let img = new Image()
+          img.src = galItems[to].src
+          slide.appendChild(img)
+        }
       }
     })
 
     document.documentElement.appendChild(galContainer)
-  }
-
-  let close = function() {
-    galContainer.remove()
-    galContent.innerHTML = ''
   }
 
   let galItems = map(el.querySelectorAll('.gal-item'), (itemEl, index) => {
