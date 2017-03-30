51f1d35b755bb29828a93147f2a1121519096556
Giaco Corsiglia <GiacoCorsiglia@gmail.com>
2017-03-30 13:25:41 -0700

    Muck around with webpack
    - babel-plugin-lodash for lodash “tree shaking”
    - source map
    - standardize indentation

--- _assets/webpack.config.js
+++ _assets/webpack.config.js
@@ -4,41 +4,43 @@ const path = require('path')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
-	context: path.resolve(__dirname, './src'),
-	entry: {
-		app: './index.js',
-	},
-	output: {
-		path: path.resolve(__dirname, '../'),
-		filename: '_script.js',
-	},
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: [/node_modules/],
-				use: [{
-					loader: 'babel-loader',
-					options: {
-						presets: ['es2015'],
-						ignore: /node_modules/,
-					},
-				}],
-			},
-			{
-				test: /\.scss$/,
-				use: ExtractTextPlugin.extract({
-					fallback: 'style-loader',
-					use: ['css-loader', 'postcss-loader', 'sass-loader',],
-				})
-			},
-		],
-	},
-	plugins: [
-		new ExtractTextPlugin('_style.css')
-	],
+  context: path.resolve(__dirname, './src'),
+  entry: {
+    app: './index.js',
+  },
+  output: {
+    path: path.resolve(__dirname, '../_site/'),
+    filename: '_script.js',
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: [/node_modules/],
+        use: [{
+          loader: 'babel-loader',
+          options: {
+            plugins: ['lodash'],
+            presets: [['es2015', { loose: true, modules: false }]],
+            ignore: /node_modules/,
+          },
+        }],
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'postcss-loader', 'sass-loader',],
+        })
+      },
+    ],
+  },
+  plugins: [
+    new ExtractTextPlugin('_style.css')
+  ],
 	// Need this for some bug nonsense
-	node: {
-		fs: 'empty',
-	},
+  node: {
+    fs: 'empty',
+  },
 }
