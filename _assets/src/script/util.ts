05cb648a925b00a6f9eb3cae7cf71bf98c590240
Giaco Corsiglia <GiacoCorsiglia@gmail.com>
2017-05-14 19:14:37 -0700

    Get overloading to work

--- _assets/src/script/util.ts
+++ _assets/src/script/util.ts
@@ -10,7 +10,7 @@ interface Dictionary<T> {
 type ListIterator<T, U> = (value: T, index: number, collection: List<T>) => U;
 type DictionaryIterator<T, U> = (value: T, key: string, collection: Dictionary<T>) => U;
 
-export function mapDict<T, U>(collection: Dictionary<T>, iteratee: DictionaryIterator<T, U>): Dictionary<U> {
+function mapDict<T, U>(collection: Dictionary<T>, iteratee: DictionaryIterator<T, U>): Dictionary<U> {
   let ret: Dictionary<U> = {};
   for (let key in collection) {
     if (collection.hasOwnProperty(key)) {
@@ -20,12 +20,12 @@ export function mapDict<T, U>(collection: Dictionary<T>, iteratee: DictionaryIte
   return ret;
 }
 
-export function map<T, U>(collection: List<T>, iteratee: ListIterator<T, U>): U[] {
+function mapList<T, U>(collection: List<T>, iteratee: ListIterator<T, U>): U[] {
   collection = collection instanceof NodeList ? [].slice.call(collection) : collection;
   return (<Array<T>>collection).map(iteratee);
 }
 
-export function eachDict<T>(collection: Dictionary<T>, iteratee: DictionaryIterator<T, void>): void {
+function eachDict<T>(collection: Dictionary<T>, iteratee: DictionaryIterator<T, void>): void {
   for (let key in collection) {
     if (collection.hasOwnProperty(key)) {
       iteratee(collection[key], key, collection);
@@ -33,7 +33,33 @@ export function eachDict<T>(collection: Dictionary<T>, iteratee: DictionaryItera
   }
 }
 
-export function each<T>(collection: List<T>, iteratee: ListIterator<T, void>): void {
+function eachList<T>(collection: List<T>, iteratee: ListIterator<T, void>): void {
   collection = collection instanceof NodeList ? [].slice.call(collection) : collection;
   (<Array<T>>collection).forEach(iteratee);
+}
+
+function isDictionary<T>(collection: any): collection is Dictionary<T> {
+  return typeof collection === 'object' && collection !== null;
+}
+
+export function map<T, U>(collection: List<T>, iteratee: ListIterator<T, U>): U[];
+export function map<T, U>(collection: Dictionary<T>, iteratee: DictionaryIterator<T, U>): Dictionary<U>;
+export function map<T, U>(collection: List<T> | Dictionary<T>, iteratee: ListIterator<T, U> | DictionaryIterator<T, U>): U[] | Dictionary<U> {
+  if (isDictionary<T>(collection)) {
+    return mapDict(collection, <DictionaryIterator<T, U>>iteratee);
+  }
+  else {
+    return mapList(collection, <ListIterator<T, U>>iteratee);
+  }
+}
+
+export function each<T>(collection: List<T>, iteratee: ListIterator<T, void>): void;
+export function each<T>(collection: Dictionary<T>, iteratee: DictionaryIterator<T, void>): void;
+export function each<T>(collection: List<T> | Dictionary<T>, iteratee: ListIterator<T, void> | DictionaryIterator<T, void>): void {
+  if (isDictionary<T>(collection)) {
+    eachDict(collection, <DictionaryIterator<T, void>>iteratee);
+  }
+  else {
+    eachList(collection, <ListIterator<T, void>>iteratee);
+  }
 }
